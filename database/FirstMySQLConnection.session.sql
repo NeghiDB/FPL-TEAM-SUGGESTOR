6d294598ef1ea2d3f981c4c432e30043fb7b0add
NeghiDB <neghianruwa@gmail.com>
2023-12-31 06:59:46 +0100

    Another Commit

--- database/FirstMySQLConnection.session.sql
+++ database/FirstMySQLConnection.session.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE FPLTeamSuggestor;
 USE FPLTeamSuggestor;
-CREATE TABLE players (
+CREATE TABLE fplteamsuggestor.players (
     player_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     position VARCHAR(2) NOT NULL,
     team VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE gameweeks (
+CREATE TABLE fplteamsuggestor.gameweeks (
     gameweek_id SERIAL PRIMARY KEY,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE TABLE player_performances (
+CREATE TABLE fplteamsuggestor.player_performances (
     gameweek_id INT REFERENCES gameweeks(gameweek_id),
     player_id INT REFERENCES players(player_id),
     points INT NOT NULL,
@@ -22,4 +22,13 @@ CREATE TABLE player_performances (
     clean_sheets INT DEFAULT 0,
     bonus_points INT DEFAULT 0,
     PRIMARY KEY (gameweek_id, player_id)
+);
+
+
+CREATE TABLE fplteamsuggestor.users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
